fed8cf572c65ee16335f3a75e5dfaf74d98937cf
mily datta <dmily270@gmail.com>
2021-12-09 21:10:30 +0600

    some update

--- src/Components/Club/Club.js
+++ src/Components/Club/Club.js
@@ -5,7 +5,7 @@ import ClubDetail from '../ClubDetail/ClubDetail';
 const Club = () => {
     const [leagues, setLeagues] = useState([]);
     useEffect(() => {
-        const url = `https://www.thesportsdb.com/api/v1/json/1/all_leagues.php`;
+        const url = `https://www.thesportsdb.com/api/v1/json/2/all_leagues.php`;
         fetch(url)
         .then(res => res.json())
         .then(data =>setLeagues(data.leagues.slice(0,12)))
